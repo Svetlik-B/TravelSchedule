b4eafed1ea2a85154fef86a554ddb1dee3217427
Svetlik-B <svetlik.igorevna@gmail.com>
2025-05-30 00:49:28 +0400

    добавить 8 сетевых сервисов API Яндекс Расписаний

---
+++ TravelSchedule/Services/NearestSettlementService.swift
@@ -0,0 +1,34 @@
+typealias NearestSettlement = Components.Schemas.NearestSettlementResponse
+
+protocol NearestSettlementServiceProtocol {
+    func getNearestSettlement(
+        lat: Double,
+        lng: Double,
+        distance: Double?
+    ) async throws -> NearestSettlement
+}
+
+final class NearestSettlementService: NearestSettlementServiceProtocol {
+    private let client: Client
+    private let apikey: String
+        
+    init(client: Client, apikey: String) {
+        self.client = client
+        self.apikey = apikey
+    }
+    
+    func getNearestSettlement(
+        lat: Double,
+        lng: Double,
+        distance: Double?
+    ) async throws -> NearestSettlement {
+        try await client.getNearestSettlement(
+            query: .init(
+                apikey: apikey,
+                lat: lat,
+                lng: lng,
+                distance: distance
+            )
+        ).ok.body.json
+    }
+}
--- TravelSchedule/TravelScheduleApp.swift
+++ TravelSchedule/TravelScheduleApp.swift
@@ -1,9 +1,6 @@
-//
-//  TravelScheduleApp.swift
-//  TravelSchedule
-//
-//  Created by Svetlana Bochkareva on 29.05.2025.
-//
+enum Constant {
+    static let apiKey = "65364b60-01c8-4836-9d57-47eff3a54212"
+}
 
 import SwiftUI
 
