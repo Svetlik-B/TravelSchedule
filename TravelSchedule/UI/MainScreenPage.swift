8d8e7a070b2f54271e9392e87c29a812bbf64310
Svetlik-B <svetlik.igorevna@gmail.com>
2025-06-18 21:41:04 +0400

    обработка ошибок

--- TravelSchedule/UI/MainScreenPage.swift
+++ TravelSchedule/UI/MainScreenPage.swift
@@ -1,10 +1,12 @@
 import SwiftUI
 
 struct MainScreenPage: View {
-    @Environment(\.colorScheme) var colorScheme
+    enum PageTab { case search, settings }
     @State private var isDark = true
+    @State private var tab = PageTab.search
+    @State private var showError: ErrorKind?
     var body: some View {
-        TabView {
+        TabView(selection: $tab) {
             VStack {
                 StationSelectionPage()
                 Spacer()
@@ -13,18 +15,50 @@ struct MainScreenPage: View {
             .tabItem {
                 Image(uiImage: .schedule)
             }
+            .tag(PageTab.search)
+
             VStack {
-                SettingsPage()
+                switch showError {
+                case .internet:
+                    Spacer()
+                    ErrorPage(kind: .internet)
+                case .server:
+                    Spacer()
+                    ErrorPage(kind: .server)
+                case nil:
+                    SettingsPage()
+                }
                 Spacer()
                 Divider()
             }
             .tabItem {
                 Image(uiImage: .settings)
             }
+            .tag(PageTab.settings)
+        }
+        .onChange(of: tab) { old, new in
+            if  old == .settings {
+                showError = nil
+            }
         }
         .tint(.primary)
         .environment(\.travelScheduleIsDarkBinding, $isDark)
         .environment(\.colorScheme, isDark ? .dark : .light)
+        .environment(\.onError) { errorKind in
+            tab = .settings
+            showError = errorKind
+        }
+    }
+}
+
+enum ErrorKind {
+    case internet
+    case server
+}
+
+extension EnvironmentValues {
+    @Entry var onError: (ErrorKind) -> Void = {
+        print("ErrorKind: \($0)")
     }
 }
 
